8f725b826f535e3ce9cff3e0b027e2d311b2154a
Elizabeth Mejia <elizabethmej77@gmail.com>
2020-01-18 14:18:24 -0500

    Subiendo un único archivo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    };
+  }
+
+  onChangeHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0
+    });
+  };
+
+  onClickHandler = () => {
+    const data = new FormData();
+    data.append('file', this.state.selectedFile);
+
+    axios.post("http://localhost:8000/upload", data, { 
+     
+      // receive two parameter endpoint url ,form data 
+      
+    })
+      .then(res => { // then print response status
+        console.log(res.statusText)
+      })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="offset-md-3 col-md-6">
+            <div className="form-group files color">
+              <label>Cargue su archivo </label>
+              <input
+                className="form-control"
+                multiple=""
+                type="file"
+                name="file"
+                onChange={this.onChangeHandler}
+              />
+            </div>
+            <button
+              type="button"
+              className="btn btn-success btn-block"
+              onClick={this.onClickHandler}
+            >
+              Cargar
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
